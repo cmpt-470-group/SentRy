e75c4a5fa92ab4f02494e2aecd8f01e3501e714e
David J. D. Hampson <davidhampson.97@gmail.com>
2019-04-12 11:13:12 -0700

    Need to input target

--- SentRyTest.py
+++ SentRyTest.py
@@ -6,11 +6,12 @@ import SentRyCompare
 import SentRyDisplay
 
 SentRNAOutputFilepath = 'E100SentRNA_models.txt'
+target = "..." # where do we get target
 sri = SR.Interpreter(SentRNAOutputFilepath)
 
 sa1 = EB.GetEBOutput()
 sa2 = sri.GetSentRNAOutput()
 
-sa3 = SentRyCompare.CompareSeqArrs(sa1, sa2)
+sa3 = SentRyCompare.CompareSeqArrs(sa1, sa2, target)
 
 SentRyDisplay.DisplayOutput(sa3)
