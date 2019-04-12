adaaf7c99c35acdd550b11478e0f726f45356540
David J. D. Hampson <davidhampson.97@gmail.com>
2019-04-12 11:08:22 -0700

    Update SentRyObjects.py

--- SentRyObjects.py
+++ SentRyObjects.py
@@ -2,11 +2,12 @@
 import RNA # pip install RNAlib-2.4.11 
 
 class Sequence:
-    def __init__(self, FE = 0, PS = "", SS = "", Name="Undefined"):
+    def __init__(self, FE = 0, PS = "", SS = "", Name="Undefined",Target="Undefined"):
         self.FreeEnergy = FE
         self.PrimaryS = PS
         self.SecondaryS = SS
         self.Name = Name
+        self.Target = target
         self.FoldAndEvaluate()
 
     def Display(self):
@@ -23,4 +24,8 @@ class Sequence:
         self.SecondaryS = ss
         self.FreeEnergy = fe
         return true;
+    
+    def hamming_distance(self, target):
+        assert len(self.SecondaryS) == len(target)
+        return sum(ch1 != ch2 for ch1, ch2 in zip(self.SecondaryS, target))
         
